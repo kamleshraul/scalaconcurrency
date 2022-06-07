73d75812836db0e92928ebc918c20422fa558a11
Kamlesh Raul <kamleshraul@gmail.com>
2022-06-07 11:58:47 +0100

    Added new println to ThreadCreation class

--- src/main/scala/concurrency/ch2/ThreadCreation.scala
+++ src/main/scala/concurrency/ch2/ThreadCreation.scala
@@ -1,7 +1,7 @@
 package concurrency.ch2
 
 object ThreadCreation extends App {
-  
+
     class MyThread extends Thread{
 
         override def run():Unit ={
@@ -12,9 +12,10 @@ object ThreadCreation extends App {
     val  t: MyThread = new MyThread
     t.start()
     // join method stops the execution of main Thread
-    // and put main thread in waiting state unitl 
+    // and put main thread in waiting state unitl
     // thread t completes is execution
     t.join()
     println("New Thread started/join")
     println("Added to vs code...")
+    println("Added using commandline ...")
 }
